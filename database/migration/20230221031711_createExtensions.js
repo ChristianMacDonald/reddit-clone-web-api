c92ced62b684f50be4ed856fbc9e49705d4d9e17
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2023-02-20 22:46:57 -0500

    Create 'uuid-ossp' extension and users table

---
+++ database/migration/20230221031711_createExtensions.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.raw('create extension if not exists "uuid-ossp"');
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.raw('drop extension if exists "uuid-ossp"');
+};
