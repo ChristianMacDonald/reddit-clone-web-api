3d6a51023d49f994afdfb17a92405c41bc3388f1
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2023-02-21 12:04:15 -0500

    Create apiRouter and use it on /api endpoint

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+const { apiRouter } = require('./routers');
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
+
+app.use('/api', apiRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
