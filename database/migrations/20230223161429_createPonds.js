a2c3da0c053568ab21dd49517b3450f2a9aab7be
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2023-02-23 12:47:19 -0500

    Add unique constraint to name column in createPonds migration file

--- database/migrations/20230223161429_createPonds.js
+++ database/migrations/20230223161429_createPonds.js
@@ -3,7 +3,11 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('ponds', table => {
+    table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
+    table.string('name').unique().notNullable();
+    table.string('description');
+  });
 };
 
 /**
@@ -11,5 +15,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('ponds');
 };
