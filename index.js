12e177c8015fa149123239a36021b5e66489fe6f
Christian MacDonald <39752557+ChristianMacDonald@users.noreply.github.com>
2023-03-02 16:03:30 -0500

    Install cors and use it on whole server

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
-
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = 4000;
 
 const { apiRouter } = require('./routers');
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
